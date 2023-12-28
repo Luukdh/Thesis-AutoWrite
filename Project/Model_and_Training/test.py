cb63731dbe6f09fef4f9a13e98f944d25221702d
Luuk-Laptop <luuk-laptop@pop-os.localdomain>
2023-12-28 03:41:50 +0100

    trial unet

--- Project/Model_and_Training/test.py
+++ Project/Model_and_Training/test.py
@@ -2,11 +2,12 @@ import torchvision
 import numpy as np
 import torch
 
-lst = np.array([[1,2,3],[4,5,6],[7,8,9]])
+lst = np.array([[1,2,3,4,5,6,7,8,9],[10,11,12,13,14,15,16,17,18],[19,20,21,22,23,24,25,26,27]])
 # lst = [[1,2,3],[4,5,6],[7,8,9]]
 
 print(lst)
 
-ten = torch.Tensor(lst).T
+ten = torch.Tensor(lst)
 
-print(ten)
+print(ten[:, 2:-2])
+print(ten.shape)
